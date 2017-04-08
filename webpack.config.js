6397894570511eeefd46eb175e858305ccadbe33
Will <crossland.w.a@gmail.com>
2017-04-08 12:14:57 +0100

    Uglify

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,23 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+
 module.exports = {
   entry: './index.js',
   output: {
     path: `${__dirname}/dist`,
     filename: 'index_bundle.js'
   },
-  plugins: [new HtmlWebpackPlugin({
-    filename: 'index.html',
-    template: './index.ejs'
-  })],
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './index.ejs'
+    })
+  ],
   module: {
     rules: [
       {
