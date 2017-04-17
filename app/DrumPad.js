273568acc6ed064aedc12b39d8eab79292c636a3
crosslandwa <crossland.w.a@gmail.com>
2017-04-17 19:14:31 +0100

    Make onscreen pad

--- app/DrumPad.js
+++ app/DrumPad.js
@@ -3,6 +3,26 @@ import React from 'react'
 import PushGridPad from './PushGridPad'
 
 const midiGain = velocity => ({ toAbsolute: () => velocity / 127, velocity: () => velocity })
+const padStyleBase = {
+  display: 'inline-block',
+  height: 64,
+  width: 64,
+  marginLeft: 5,
+  marginRight: 5,
+  backgroundColor: '#c7ccce',
+  borderRadius: 10,
+  borderWidth: 2,
+  borderStyle: 'solid',
+  borderColor: '#333333',
+  cursor: 'pointer'
+}
+
+const padStyleActive = {
+  opacity: 0.9,
+  boxShadow: 'inset 0px 0px 50px blue',
+  borderColor: 'rgba(100, 100, 100, 0.3)'
+}
+
 
 class DrumPad extends React.Component {
   constructor(props) {
@@ -34,7 +54,10 @@ class DrumPad extends React.Component {
           playWithVelocity={this.playWithVelocity}
           rgb={[250, 250, 0]}
         />
-        <div>Playing {this.props.url}</div>
+        <div
+          onClick={() => this.playWithVelocity(100)}
+          style={Object.assign({}, padStyleBase, this.state.velocity ? padStyleActive : {})}
+        />
       </div>
     )
   }
