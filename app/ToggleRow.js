21bf908606eec3882194f2cbc656e804bad7d89e
Will <crossland.w.a@gmail.com>
2017-04-26 23:11:42 +0100

    Make ToggleRow a simple render function
    No longer needs to be stateful component

--- app/ToggleRow.js
+++ app/ToggleRow.js
@@ -4,46 +4,25 @@ import PushGridPad from './PushGridPad'
 import DomGridPad from './DomGridPad'
 import { connect } from 'react-redux'
 
-class ToggleRow extends React.Component {
-  constructor (props) {
-    super(props)
-    this.toggle = this.toggle.bind(this)
-    this.state = {
-      on: Array(8).fill(false)
-    }
-  }
-
-  toggle (index) {
-    this.setState(prevState => {
-      let on = prevState.on.slice()
-      on[index] = !on[index]
-      return { on }
-    })
-  }
-
-  render () {
-    const {gridRow, onClick, on} = this.props
-    return (
-      <div className='toggleRow'>
-        {gridRow().map((pad, index) => (
-          <PushGridPad
-            key={index}
-            velocity={on[index] ? 120 : 0}
-            pad={pad}
-            padPressed={() => onClick(index)}
-          />
-        ))}
-        {gridRow().map((pad, index) => (
-          <DomGridPad
-            key={index}
-            active={on[index]}
-            padPressed={() => onClick(index)}
-          />
-        ))}
-      </div>
-    )
-  }
-}
+const ToggleRow = ({gridRow, onClick, on}) => (
+  <div className='toggleRow'>
+    {gridRow().map((pad, index) => (
+      <PushGridPad
+        key={index}
+        velocity={on[index] ? 120 : 0}
+        pad={pad}
+        padPressed={() => onClick(index)}
+      />
+    ))}
+    {gridRow().map((pad, index) => (
+      <DomGridPad
+        key={index}
+        active={on[index]}
+        padPressed={() => onClick(index)}
+      />
+    ))}
+  </div>
+)
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
   onClick: index => {
