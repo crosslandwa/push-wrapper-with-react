b02818c4a1fd5a3260cc7f71a7623ffe8c7f2845
crosslandwa <crossland.w.a@gmail.com>
2017-05-27 15:19:57 +0100

    Refactor generation of black keys row

--- app/voices/ChromaticKeyboard.js
+++ app/voices/ChromaticKeyboard.js
@@ -9,44 +9,17 @@ const midiNoteToF = note => 440.0 * Math.pow(2, (note - 69.0) / 12.0)
 const ChromaticKeyboard = ({voice, basePitch, blackRow, whiteRow}) => (
   <div>
     <div>
-      <DomGridPad key={0} />
-      <ChromaticSamplePlayerContainer
-        key={1}
-        voice={voice}
-        rate={midiNoteToF(1 + basePitch) / midiNoteToF(basePitch)}
-        pad={blackRow[1]}
-        rgb={Colours.black}
-      />
-      <ChromaticSamplePlayerContainer
-        key={2}
-        voice={voice}
-        rate={midiNoteToF(3 + basePitch) / midiNoteToF(basePitch)}
-        pad={blackRow[2]}
-        rgb={Colours.black}
-      />
-      <DomGridPad key={3} />
-      <ChromaticSamplePlayerContainer
-        key={4}
-        voice={voice}
-        rate={midiNoteToF(6 + basePitch) / midiNoteToF(basePitch)}
-        pad={blackRow[4]}
-        rgb={Colours.black}
-      />
-      <ChromaticSamplePlayerContainer
-        key={5}
-        voice={voice}
-        rate={midiNoteToF(8 + basePitch) / midiNoteToF(basePitch)}
-        pad={blackRow[5]}
-        rgb={Colours.black}
-      />
-      <ChromaticSamplePlayerContainer
-        key={6}
-        voice={voice}
-        rate={midiNoteToF(10 + basePitch) / midiNoteToF(basePitch)}
-        pad={blackRow[6]}
-        rgb={Colours.black}
-      />
-      <DomGridPad key={7} />
+      {[0, 1, 3, 0, 6, 8, 10, 0].map((offset, index) => (
+        offset
+          ? <ChromaticSamplePlayerContainer
+            key={index}
+            voice={voice}
+            rate={midiNoteToF(offset + basePitch) / midiNoteToF(basePitch)}
+            pad={blackRow[1]}
+            rgb={Colours.black}
+            />
+        : <DomGridPad key={index} />
+      ))}
     </div>
     <div>
       {[0, 2, 4, 5, 7, 9, 11, 12].map((offset, index) => (
