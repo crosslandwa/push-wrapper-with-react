44ab3312821e64bb203c76838351c55ac5b08fb2
Will <crossland.w.a@gmail.com>
2017-06-11 12:09:37 +0100

    Remove track selection code from TrackSelectContainer

--- app/voices/TrackSelectContainer.js
+++ app/voices/TrackSelectContainer.js
@@ -3,21 +3,17 @@ import React from 'react'
 import { connect } from 'react-redux'
 import DomGridPad from '../push/DomGridPad'
 import PushGridPad from '../push/PushGridPad'
-import { selectTrack } from '../ui/actions'
 import { Colours } from '../push/colours'
 
-const playingColour = velocity => velocity > 0 ? Colours.turquoise : false
-const selectedColour = selected => selected ? Colours.orange : false
-
-const TrackSelectContainer = ({ velocity = 0, padPressed, padReleased, pad, selected}) => (
+const TrackSelectContainer = ({ velocity = 0, padPressed, padReleased, pad}) => (
   <div style={{display: 'inline-block'}} >
     <DomGridPad
       padPressed={padPressed}
-      active={(velocity > 0) || selected}
-      rgb={playingColour(velocity) || selectedColour(selected)}
+      active={(velocity > 0)}
+      rgb={Colours.turquoise}
     />
     <PushGridPad
-      rgb={playingColour(velocity) || selectedColour(selected) || Colours.off}
+      rgb={velocity > 0 ? Colours.turquoise : Colours.off}
       pad={pad}
       padPressed={padPressed}
     />
@@ -25,17 +21,10 @@ const TrackSelectContainer = ({ velocity = 0, padPressed, padReleased, pad, sele
 )
 
 export default connect(
-  ({ entities: { voices, tracks }, ui: { selectedTrackId } }, { trackId }) => {
+  ({ entities: { voices, tracks } }, { trackId }) => {
     const voiceId = tracks.byId[trackId].voiceId
     return {
-      selected: selectedTrackId === trackId,
       velocity: voices.byId[voiceId].velocity,
     }
-  },
-  (dispatch, { trackId, padPressed }) => ({
-    padPressed (velocity) {
-      dispatch(selectTrack(trackId))
-      padPressed && padPressed(velocity)
-    }
-  })
+  }
 )(TrackSelectContainer)
