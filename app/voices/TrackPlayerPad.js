d6892b5c53a4421ecde7f278dcdc5286a93be873
Will <crossland.w.a@gmail.com>
2017-06-11 17:05:09 +0100

    Replace velocity > 0 with active

--- app/voices/TrackPlayerPad.js
+++ app/voices/TrackPlayerPad.js
@@ -6,15 +6,15 @@ import DomGridPad from '../push/DomGridPad'
 import PushGridPad from '../push/PushGridPad'
 import { Colours } from '../push/colours'
 
-const TrackPlayerPad = ({ velocity = 0, padPressed, padReleased, pad}) => (
+const TrackPlayerPad = ({ active, padPressed, padReleased, pad}) => (
   <div style={{display: 'inline-block'}} >
     <DomGridPad
       padPressed={padPressed}
-      active={(velocity > 0)}
+      active={active}
       rgb={Colours.turquoise}
     />
     <PushGridPad
-      rgb={velocity > 0 ? Colours.turquoise : Colours.off}
+      rgb={active ? Colours.turquoise : Colours.off}
       pad={pad}
       padPressed={padPressed}
     />
@@ -24,7 +24,7 @@ const TrackPlayerPad = ({ velocity = 0, padPressed, padReleased, pad}) => (
 const mapStateToProps = ({ entities: { voices, tracks } }, { trackId }) => {
   const voiceId = tracks.byId[trackId].voiceId
   return {
-    velocity: voices.byId[voiceId].velocity,
+    active: voices.byId[voiceId].velocity > 0,
   }
 }
 
