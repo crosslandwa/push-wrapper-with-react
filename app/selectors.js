038b8184e30d09c9cb4818fa89b338c87c48dce2
crosslandwa <crossland.w.a@gmail.com>
2017-06-19 08:29:13 +0100

    Organise selectors

--- app/selectors.js
+++ app/selectors.js
@@ -1,28 +1,33 @@
 import { createSelector } from 'reselect'
 
-export const currentTrack = state => state.entities.tracks.byId[state.ui.selectedTrackId]
-
+// ############ PATTERN
 const currentPatternId = state => state.sequencer.patternId
-
 const patternsSelector = state => state.entities.patterns
-
-const voicesSelector = state => state.entities.voices
-
-const samplesSelector = state => state.entities.samples
-
-const stepsSelector = state => state.entities.steps
 
 export const patternIds = state => state.entities.patterns.allIds
 
-export const sampleIds = createSelector(
-  samplesSelector,
-  samples => samples.allIds
-)
+export const patternSelector = (state, id) => patternsSelector(state).byId[id]
 
 export const currentPattern = createSelector(
   [patternsSelector, currentPatternId],
   (patterns, id) => patterns.byId[id]
 )
+
+// ############ TRACK
+export const currentTrack = state => state.entities.tracks.byId[state.ui.selectedTrackId]
+
+// ############ TRACK
+const tracksSelector = state => state.entities.tracks
+
+export const trackSelector = (state, trackId) => tracksSelector(state).byId[trackId]
+
+// ############ STEPS
+const stepsSelector = state => state.entities.steps
+
+export const stepSelector = (state, stepId) => stepsSelector(state).byId[stepId]
+
+// ############ VOICE
+const voicesSelector = state => state.entities.voices
 
 export const currentVoice = createSelector(
   currentTrack,
@@ -32,28 +37,28 @@ export const currentVoice = createSelector(
   }
 )
 
-const tracksSelector = state => state.entities.tracks
-
-export const trackSelector = (state, trackId) => tracksSelector(state).byId[trackId]
-
-export const stepSelector = (state, stepId) => stepsSelector(state).byId[stepId]
-
-export const sampleSelector = (state, sampleId) => samplesSelector(state).byId[sampleId]
-
-export const patternSelector = (state, id) => patternsSelector(state).byId[id]
-
 export const voiceForTrack = createSelector(
   trackSelector,
   voicesSelector,
   (track, voices) => voices.byId[track.voiceId]
 )
 
-export const sampleForTrack = createSelector(
-  [voiceForTrack, samplesSelector],
-  (voice, samples) => samples.byId[voice.sampleId]
+// ############ SAMPLE
+const samplesSelector = state => state.entities.samples
+
+export const sampleIds = createSelector(
+  samplesSelector,
+  samples => samples.allIds
 )
+
+export const sampleSelector = (state, sampleId) => samplesSelector(state).byId[sampleId]
 
 export const currentSample = createSelector(
   [currentVoice, samplesSelector],
   (currentVoice, samples) => samples.byId[currentVoice.sampleId]
+)
+
+export const sampleForTrack = createSelector(
+  [voiceForTrack, samplesSelector],
+  (voice, samples) => samples.byId[voice.sampleId]
 )
