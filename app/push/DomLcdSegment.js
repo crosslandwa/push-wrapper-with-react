f630591c7eb7bcd7fc138994c9bcee88f72b8d45
crosslandwa <crossland.w.a@gmail.com>
2017-06-22 16:20:09 +0100

    Layout LCD segments with flexbox

--- app/push/DomLcdSegment.js
+++ app/push/DomLcdSegment.js
@@ -3,15 +3,14 @@ import React from 'react'
 import { Colours } from './colours'
 
 const style = {
-  display: 'table-cell',
   textAlign: 'center',
-  verticalAlign: 'middle',
   fontFamily: 'monospace',
-  width: '12.5%',
+  width: '12%',
   borderRadius: 4,
   boxShadow: 'inset 0px 0px 10px rgba(25, 25, 25, 0.5)',
   fontSize: '11px',
-  height: '1.75em'
+  lineHeight: '2em',
+  height: '2em'
 }
 
 const DomLcdSegment = ({text}) => <div style={style}>{text}</div>
--- app/ui/LCD.js
+++ app/ui/LCD.js
@@ -9,31 +9,30 @@ const paddedRows = rows => rows.concat(emptyRows).slice(0, 4)
 const paddedRow = row => row.concat(emptyRow).slice(0, 8)
 
 const style = {
-  display: 'table',
+  display: 'flex',
   width: '100%',
+  height: '100%',
+  flexWrap: 'wrap',
+  justifyContent: 'space-around',
   backgroundColor: '#e16301',
   borderStyle: 'solid',
   borderWidth: 2,
   borderRadius: 4,
-  boxShadow: 'inset 0px 0px 20px rgba(25, 25, 25, 1)',
+  boxShadow: 'inset 0px 0px 20px rgba(25, 25, 25, 1)'
 }
 
 const LCD = ({data = [], pushLcdSegmentsRow}) => (
   <div style={style}>
-    {paddedRows(data).map((row, rowIndex) => (
-      <div key={rowIndex} style={{display: 'table-row'}}>
-        {paddedRow(row).map((text, colIndex) => (
-          <DomLcdSegment key={colIndex} text={text} />
-        ))}
-        {paddedRow(row).map((text, colIndex) => (
-          <PushLcdSegment
-            key={colIndex}
-            lcdSegment={pushLcdSegmentsRow(3 - rowIndex)[colIndex]}
-            text={text}
-          />
-        ))}
-      </div>
-    ))}
+    {paddedRows(data).map((row, rowIndex) => paddedRow(row).map((text, colIndex) => (
+      <DomLcdSegment key={colIndex} text={text} />
+    )))}
+    {paddedRows(data).map((row, rowIndex) => paddedRow(row).map((text, colIndex) => (
+      <PushLcdSegment
+        key={colIndex}
+        lcdSegment={pushLcdSegmentsRow(3 - rowIndex)[colIndex]}
+        text={text}
+      />
+    )))}
   </div>
 )
 
