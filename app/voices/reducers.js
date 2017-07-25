d953be6f38191d7cde5c567b8ea4ac98df22d737
crosslandwa <crossland.w.a@gmail.com>
2017-07-25 19:04:48 +0100

    Refactor to reduce updateParam instead of explicit functions per parameter

--- app/voices/reducers.js
+++ app/voices/reducers.js
@@ -14,13 +14,13 @@ export default function voices (state = initialState, action) {
     case 'KIT_CREATE':
       return createVoices(state, action.voiceIds, action.sampleIds)
     case 'VOICE_SWITCH_SAMPLE':
-      return switchSample(state, action.voiceId, action.sampleId)
+      return updateParam(state, action.voiceId, 'sampleId', action.sampleId)
     case 'VOICE_UPDATE_PITCH':
-      return updatePitch(state, action.id, action.pitch)
+      return updateParam(state, action.id, 'pitch', action.pitch)
     case 'VOICE_UPDATE_DECAY':
-      return updateDecay(state, action.id, action.decay)
+      return updateParam(state, action.id, 'decay', action.decay)
     case 'VOICE_UPDATE_FILTER_F':
-      return updateFilterF(state, action.id, action.filterAmount)
+      return updateParam(state, action.id, 'filterAmount', action.filterAmount)
     case 'VOICE_UPDATE_VOLUME':
       return updateParam(state, action.id, 'midiVolume', action.midiVolume)
   }
@@ -49,30 +49,6 @@ function createVoices (state, ids, sampleIds) {
     }, clone(state.byId)),
     allIds: state.allIds.concat(ids)
   }
-}
-
-function switchSample(state, id, sampleId) {
-  const updated = clone(state)
-  updated.byId[id].sampleId = sampleId
-  return updated
-}
-
-function updatePitch(state, id, pitch) {
-  const updated = clone(state)
-  updated.byId[id].pitch = pitch
-  return updated
-}
-
-function updateDecay(state, id, decay = 100) {
-  const updated = clone(state)
-  updated.byId[id].decay = decay
-  return updated
-}
-
-function updateFilterF(state, id, filterAmount = 100) {
-  const updated = clone(state)
-  updated.byId[id].filterAmount = filterAmount
-  return updated
 }
 
 function updateParam(state, id, param, value) {
