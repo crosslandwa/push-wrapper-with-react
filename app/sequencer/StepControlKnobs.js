e6b1d6b8064a2104e36c1c1fc80268751a45a174
will.crossland <crossland.w.a@gmail.com>
2017-08-01 19:02:21 +0100

    Refactor to inline creation of bound Knob into StepControlKnobs

--- app/sequencer/StepControlKnobs.js
+++ app/sequencer/StepControlKnobs.js
@@ -1,23 +1,34 @@
 'use strict'
 import React from 'react'
 import { connect } from 'react-redux'
-import StepDecayControl from './StepDecayControl'
-import StepPitchControl from './StepPitchControl'
-import StepVelocityControl from './StepVelocityControl'
+import { bindActionCreators } from 'redux'
 import ChannelKnobs from '../ui/ChannelKnobs'
+import ClickyDraggy from '../push/ClickyDraggy'
 import DomKnob from '../push/DomKnob'
+import PushKnob from '../push/PushKnob'
+import { changeStepDecayBy, changeStepPitchBy, changeStepVelocityBy } from './actions'
+
+const Knob = connect(
+  null,
+  (dispatch, ownProps) => bindActionCreators(ownProps.actions, dispatch)
+)((props) => (
+  <ClickyDraggy {...props} >
+    <DomKnob />
+    <PushKnob {...props} />
+  </ClickyDraggy>
+))
 
 const StepControlKnobs = ({knobs}) => {
   return (
     <ChannelKnobs>
-      <StepPitchControl knob={knobs[0]} />
+      <Knob knob={knobs[0]} actions={{ onTurned: changeStepPitchBy }}/>
       <DomKnob />
-      <StepDecayControl knob={knobs[2]} />
-      <DomKnob />
-      <DomKnob />
+      <Knob knob={knobs[2]} actions={{ onTurned: changeStepDecayBy }}/>
       <DomKnob />
       <DomKnob />
-      <StepVelocityControl knob={knobs[7]} />
+      <DomKnob />
+      <DomKnob />
+      <Knob knob={knobs[7]} actions={{ onTurned: changeStepVelocityBy }}/>
     </ChannelKnobs>
   )
 }
