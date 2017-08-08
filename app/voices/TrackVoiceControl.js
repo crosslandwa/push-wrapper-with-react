62d9731535e0d3453c42366317c1ed930c382d97
will.crossland <crossland.w.a@gmail.com>
2017-08-08 17:40:35 +0100

    Remove reset voice params when twisting knobs while delete held

--- app/voices/TrackVoiceControl.js
+++ app/voices/TrackVoiceControl.js
@@ -6,20 +6,13 @@ import ChannelKnobs from '../ui/ChannelKnobs'
 import ClickyDraggy from '../push/ClickyDraggy'
 import DomKnob from '../push/DomKnob'
 import PushKnob from '../push/PushKnob'
-import { resetPitch, updatePitch } from './actions'
-import { resetDecay, updateDecay } from './actions'
-import { resetFilterFrequency, updateFilterFrequency } from './actions'
-import { resetVolume, updateVolume } from './actions'
+import { updateDecay, updateFilterFrequency, updatePitch, updateVolume } from './actions'
 import { switchSample } from './actions'
 import { startSampleSelection, stopSampleSelection } from '../ui/actions'
 import { modifiersDeleteSelector } from '../selectors'
 
 const mergeProps = (state, { dispatch }, ownProps) => {
-  const actions = Object.assign(
-    {},
-    modifiersDeleteSelector(state) ? ownProps.resetActions || {} : ownProps.actions
-  )
-  return Object.assign({}, ownProps, bindActionCreators(actions, dispatch))
+  return Object.assign({}, ownProps, bindActionCreators(ownProps.actions, dispatch))
 }
 
 const Knob = connect((state) => state, null, mergeProps)((props) => (
@@ -32,14 +25,14 @@ const Knob = connect((state) => state, null, mergeProps)((props) => (
 const TrackVoiceControl = ({knobs}) => {
   return (
     <ChannelKnobs>
-      <Knob knob={knobs[0]} actions={{ onTurned: updatePitch }} resetActions={{ onTurned: resetPitch }} />
+      <Knob knob={knobs[0]} actions={{ onTurned: updatePitch }} />
       <Knob knob={knobs[1]} actions={{ onTurned: switchSample, onPressed: startSampleSelection, onReleased: stopSampleSelection }}/>
-      <Knob knob={knobs[2]} actions={{ onTurned: updateDecay }} resetActions={{ onTurned: resetDecay }} />
+      <Knob knob={knobs[2]} actions={{ onTurned: updateDecay }} />
       <DomKnob />
       <DomKnob />
       <DomKnob />
-      <Knob knob={knobs[6]} actions={{ onTurned: updateFilterFrequency }} resetActions={{ onTurned: resetFilterFrequency }} />
-      <Knob knob={knobs[7]} actions={{ onTurned: updateVolume }} resetActions={{ onTurned: resetVolume }} />
+      <Knob knob={knobs[6]} actions={{ onTurned: updateFilterFrequency }} />
+      <Knob knob={knobs[7]} actions={{ onTurned: updateVolume }} />
     </ChannelKnobs>
   )
 }
