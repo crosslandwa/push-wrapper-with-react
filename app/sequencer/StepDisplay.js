56818a0bab89d3249826c12e44a1aee1b1e272a4
will.crossland <crossland.w.a@gmail.com>
2017-08-11 08:51:36 +0100

    Start refactoring StepDisplay to separate individual StepPads

--- app/sequencer/StepDisplay.js
+++ app/sequencer/StepDisplay.js
@@ -21,39 +21,64 @@ const displayRgb = ({isCurrentStep, isDisplayedInEditWindow, isSelected, hasNote
   return hasNote ? fadeEffect(Colours.blue, velocity) : Colours.off
 }
 
-const StepDisplay = ({pads, numberOfSteps, onClick, onRelease = () => {}, recording, stepData}) => (
-  <div style={style} >
-    {pads.map((pad, stepNumber) => stepNumber < numberOfSteps
-      ? (
-        <DomGridPad
-          key={stepNumber}
-          active={stepData[stepNumber].isCurrentStep || stepData[stepNumber].hasNote}
-          rgb={displayRgb(stepData[stepNumber], recording, domFade)}
-          padPressed={() => onClick(stepNumber, stepData[stepNumber].id)}
-          padReleased={() => onRelease(stepData[stepNumber].id)}
-        >
-          <PushGridPad
-            key={stepNumber}
-            rgb={displayRgb(stepData[stepNumber], recording, fade)}
-            pad={pad}
-            padPressed={() => onClick(stepNumber, stepData[stepNumber].id)}
-            padReleased={() => onRelease(stepData[stepNumber].id)}
-          />
-        </DomGridPad>
-      )
-      : (
-        <DomGridPad rgb={Colours.black} active={true} key={stepNumber}>
-          <PushGridPad rgb={Colours.black} pad={pad} />
-        </DomGridPad>
-      )
-    )}
-  </div>
-)
+class StepPad extends React.Component {
+  render () {
+    const { onClick, onRelease, pad, recording, stepData, stepNumber } = this.props
+    return (
+      <DomGridPad
+        active={stepData.isCurrentStep || stepData.hasNote}
+        rgb={displayRgb(stepData, recording, domFade)}
+        padPressed={() => onClick(stepNumber, stepData.id)}
+        padReleased={() => onRelease(stepData.id)}
+      >
+        <PushGridPad
+          rgb={displayRgb(stepData, recording, fade)}
+          pad={pad}
+          padPressed={() => onClick(stepNumber, stepData.id)}
+          padReleased={() => onRelease(stepData.id)}
+        />
+      </DomGridPad>
+    )
+  }
+}
+
+const mapStateToStepProps = (state, ownProps) => ({})
+const ConnectedStepPad = connect(mapStateToStepProps)(StepPad)
+
+class StepDisplay extends React.Component {
+  render () {
+    const {pads, numberOfSteps, onClick, onRelease = () => {}} = this.props
+    return (
+      <div style={style} >
+        {pads.map((pad, stepNumber) => stepNumber < numberOfSteps
+          ? (
+            <ConnectedStepPad
+              key={stepNumber}
+              stepData={this.props.stepData[stepNumber]}
+              onClick={this.props.onClick}
+              onRelease={onRelease}
+              pad={pad}
+              stepNumber={stepNumber}
+            />
+          )
+          : (
+            <DomGridPad rgb={Colours.black} active={true} key={stepNumber}>
+              <PushGridPad rgb={Colours.black} pad={pad} />
+            </DomGridPad>
+          )
+        )}
+      </div>
+    )
+  }
+
+  // shouldComponentUpdate(nextProps) {
+  //   return this.props.numberOfSteps !== nextProps.numberOfSteps
+  // }
+}
 
 const mapStateToProps = (state, { pads, trackId }) => {
   const track = trackSelector(state, trackId)
-  const allSelectedSteps = selectedSteps(state)
-  const selectedStepIds = allSelectedSteps.map(step => step.id)
+  const selectedStepIds = selectedSteps(state).map(step => step.id)
   return {
     numberOfSteps: track.numberOfSteps,
     recording: isRecording(state),
