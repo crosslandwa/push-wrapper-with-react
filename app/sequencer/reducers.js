6ab4be3ea98e597518f291678f3ba591d4dcad9e
will.crossland <crossland.w.a@gmail.com>
2017-08-15 12:42:43 +0100

    Use object spread instead of Object.assign

--- app/sequencer/reducers.js
+++ app/sequencer/reducers.js
@@ -29,45 +29,40 @@ export default function sequencer (state = initialSequencerState, action) {
       return action.ids.reduce((state, id) => unselectStep(state, id), state)
     case 'SEQUENCER_ADVANCE_STEP':
       const { currentSteps, nextSteps } = action
-      return Object.assign({}, state, { currentSteps, lastStepTimeMs: action.nowMs, nextSteps })
+      return { ...state, currentSteps, lastStepTimeMs: action.nowMs, nextSteps }
     case 'SEQUENCER_NEXT_STEP':
-      return Object.assign({}, state, { nextSteps: emptyStepNumbers().map(x => action.stepNumber) })
+      return { ...state, nextSteps: emptyStepNumbers().map(x => action.stepNumber) }
     case 'SEQUENCER_START':
-      return Object.assign({}, state, { currentSteps: emptyStepNumbers(),  nextSteps: emptyStepNumbers().map(x => action.stepNumber), playing: true })
+      return { ...state, currentSteps: emptyStepNumbers(),  nextSteps: emptyStepNumbers().map(x => action.stepNumber), playing: true }
     case 'SEQUENCER_STOP':
-      return Object.assign({}, state, { currentSteps: emptyStepNumbers(), nextSteps: emptyStepNumbers(), playing: false })
+      return { ...state, currentSteps: emptyStepNumbers(), nextSteps: emptyStepNumbers(), playing: false }
     case 'SEQUENCER_ARM':
-      return Object.assign({}, state, { recording: true })
+      return { ...state, recording: true }
     case 'SEQUENCER_DISARM':
-      return Object.assign({}, state, { recording: false })
+      return { ...state, recording: false }
     case 'PATTERN_CREATE_THEN_SELECT':
     case 'PATTERN_SELECT':
-      return Object.assign({}, state, { patternId: action.id })
+      return { ...state, patternId: action.id }
     case 'SEQUENCER_UPDATE_BPM':
-      return Object.assign({}, state, { bpm: action.bpm })
+      return { ...state, bpm: action.bpm }
     case 'SEQUENCER_UPDATE_SWING':
-      return Object.assign({}, state, { swing: action.swing })
+      return { ...state, swing: action.swing }
   }
   return state
 }
 
 function selectStep (state, {stepId})  {
-  const currentlySelected = state.selectedStepIds.slice()
-  return Object.assign({}, state, {
-    selectedStepIds: currentlySelected.concat(
-      currentlySelected.includes(stepId) ? [] : stepId
-    )
-  })
+  const current = state.selectedStepIds.slice()
+  const selectedStepIds = current.concat(current.includes(stepId) ? [] : stepId)
+  return { ...state, selectedStepIds }
 }
 
 function unselectAllSteps (state) {
-  return Object.assign({}, state, { selectedStepIds: initialSequencerState.selectedStepIds })
+  return { ...state, selectedStepIds: initialSequencerState.selectedStepIds }
 }
 
 function unselectStep (state, stepId)  {
-  return Object.assign({}, state, {
-    selectedStepIds: state.selectedStepIds.slice().filter(id => id !== stepId)
-  })
+  return { ...state, selectedStepIds: state.selectedStepIds.slice().filter(id => id !== stepId) }
 }
 
 function deleteModeOn (state, trackId) {
