1e1f5eea1ae16ddbc50e0db9401b9a3fd6a4133f
will.crossland <crossland.w.a@gmail.com>
2017-08-15 09:17:19 +0100

    Refactor to use object spread instead of Object.assign

--- app/ui/reducers.js
+++ app/ui/reducers.js
@@ -6,16 +6,10 @@ const initialUiState = {
 export default function push (state = initialUiState, action) {
   switch (action.type) {
     case 'UI_SELECT_TRACK':
-      return Object.assign({},
-        state,
-        { selectedTrackId: action.trackId }
-      )
+      return { ...state, selectedTrackId: action.trackId }
     case 'UI_SAMPLE_SELECT_START':
     case 'UI_SAMPLE_SELECT_STOP':
-    return Object.assign({},
-      state,
-      { sampleSelectionOn: action.type === 'UI_SAMPLE_SELECT_START' }
-    )
+      return { ...state, sampleSelectionOn: action.type === 'UI_SAMPLE_SELECT_START' }
   }
   return state
 }
